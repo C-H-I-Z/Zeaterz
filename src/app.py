8ac0247859137d84165751599f2f90a72306bacf
en-kazvu <ellan@kazvu.com>
2026-05-18 19:47:40 -0700

    Add about and help pages to existing system, need to refactor the images

--- src/app.py
+++ src/app.py
@@ -147,6 +147,13 @@ def extract():
   finally:
       os.unlink(tmp_path)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
 
 # if __name__ == "__main__":
 #   print("\n" + "=" * 50)
